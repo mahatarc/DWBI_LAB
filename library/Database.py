c4aa39c9c07ba28709c23e4399ed15fb3ea40631
mahatarc <archanamahat7@gmail.com>
2024-12-28 21:17:23 +0545

    Create Class for database connection

--- library/Database.py
+++ library/Database.py
@@ -1,17 +1,32 @@
 import mysql.connector
-
+import pandas as pd
+from Variables import Variables
 class Database:
-    def __init__(self):
-        pass
+    def __init__(self,database):
+        try:
+            self.connection = mysql.connector.connect(
+                host = Variables("host").get_variable(),
+                port = Variables("port").get_variable(),
+                user = Variables("user").get_variable(),
+                password = Variables("password").get_variable(),
+                database= database
+            )
+            if self.connection.is_connected():
+                print("Successfully connected to MySQL!")
+        except mysql.connector.Error as e:
+            print(f"Error connecting to MySQL: {e}")
 
-    def connect(self):
-        pass
 
-    def execute_query(self):
-        pass
-
+    def execute_query(self,select_query):
+        df= pd.read_sql(select_query,self.connection)
+        return df
+    
     def disconnect(self):
         pass
 
     def fetch(self):
         pass
+      
+db= Database("DWBI")
+table = db.execute_query("SELECT * FROM STUDENT")
+print(table.head())
--- library/Variables.py
+++ library/Variables.py
@@ -2,18 +2,17 @@ import json
 
 class Variables:
     def __init__(self,variable_name):
-        self.path = "C:/Users/achar/DWBI/config/config.cfg"
+        self.path = "D:\DWBI_Practical\config\config.cfg"
         self.name = variable_name
 
     def get_variable(self):
         with open(self.path,"r") as file:
 
             file_content = json.loads(file.read())
-            print(file_content)
             return file_content[self.name]
 
 
-var = Variables("DATABASE_NAME")
+var = Variables("database")
 
 
 print(var.get_variable())
